181026fb7fc5a1953dc0ae436577b5ffa558af18
hilldaniel4 <hill_daniel4@columbusstate.edu>
2022-03-27 23:39:35 +0000

    Added checkout.html, confirm.html, shop.html.

--- home.py
+++ home.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 
+#Renders Home page.
 @app.route("/")
 def index():
     return render_template('index.html')
+
+#Renders Shop page.
+@app.route("/shop")
+def shop():
+    return render_template('shop.html')
+
+#Renders Checkout page
+@app.route('/checkout')
+def checkout():
+    return render_template('checkout.html')
+
+#Renders Order Confirmation page
+@app.route('/confirm')
+def confirm():
+    return render_template('confirm.html')
